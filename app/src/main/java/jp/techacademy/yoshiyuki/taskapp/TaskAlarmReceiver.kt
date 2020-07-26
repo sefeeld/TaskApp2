c5b3695eb583c2dc661398f06fc8368d888a8df8
sefeeld <sefeeld@yahoo.co.jp>
2020-07-26 18:50:40 +0900

    レッスン６タスク管理アプリ

---
+++ app/src/main/java/jp/techacademy/yoshiyuki/taskapp/TaskAlarmReceiver.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.yoshiyuki.taskapp
+
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
+class TaskAlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("TaskApp", "onReceive")
+    }
+}
